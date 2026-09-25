7de4e7815f258cb52534213038c5919c7fb6e197
zevaaa <sebastiancordova172011@gmail.com>
2026-09-25 19:00:04 -0400

    Update material3 dependency version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
